d8e151b68a7199b845d35593b57812ee937c9135
kwedwick <kwedwick@gmail.com>
2023-04-02 15:36:05 -0500

    db context created, category model created, category controller and view created and being rendered from db

--- BulkyBookWeb/Program.cs
+++ BulkyBookWeb/Program.cs
@@ -1,7 +1,14 @@
+using BulkyBookWeb.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Add services to the container before you build the app
 builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
+builder.Services.AddDbContext<ApplicationDBContext>(options => options.UseSqlServer(
+    builder.Configuration.GetConnectionString("DefaultConnection")));
+
 
 var app = builder.Build();
 
